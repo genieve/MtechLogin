9135a4fa85a3dac2077beb9359dfd6c760fc93d2
Alexis <alexisgenieve@gmail.com>
2023-02-03 10:05:32 -0700

    Push Login to GitHub

--- Login/ViewController.swift
+++ Login/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var forgotPasswordButton: UIButton!
+    @IBOutlet weak var forgotUsernameButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let sender = sender as? UIButton else {return}
+        if sender == forgotPasswordButton {
+            segue.destination.navigationItem.title = "Forgot Password"
+        } else if sender == forgotUsernameButton {
+            segue.destination.navigationItem.title = "Forgot Username"
+        } else {
+            segue.destination.navigationItem.title = usernameTextField.text
+        }
+    }
+    
+    @IBAction func forgotUsernameAction(_ sender: Any) {
+        performSegue(withIdentifier: "ForgottenUsernameOrPassword", sender: forgotUsernameButton)
+    }
+    @IBAction func forgotPasswordAction(_ sender: Any) {
+        performSegue(withIdentifier: "ForgottenUsernameOrPassword", sender: forgotPasswordButton)
+    }
+    
 }
 
